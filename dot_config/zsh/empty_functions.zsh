1ae0458845cc6d93e2cba8796ef0b9a685e94620
Danny Fowler <danny.fowler@nextroll.com>
2024-11-05 15:51:38 -0800

    feat: add git helper functions

--- dot_config/zsh/empty_functions.zsh
+++ dot_config/zsh/empty_functions.zsh
@@ -2,3 +2,30 @@
 chez() {
     chezmoi "$@"
 }
+
+# Create and switch to a new branch
+git_new() {
+    git checkout -b "$1"
+    git push -u origin "$1"
+}
+
+# Interactive rebase with specified number of commits
+git_ri() {
+    git rebase -i HEAD~"$1"
+}
+
+# Show git history for a specific file
+git_history() {
+    git log --follow -p -- "$1"
+}
+
+# Show most recently modified branches
+git_recent_branches() {
+    git for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)'
+}
+
+# Find commits by commit message
+gfind() {
+    git log --all --grep="$1"
+}
+
