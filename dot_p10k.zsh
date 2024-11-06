567abf536076a207f9736979c06718d7c5d4a16d
Danny Fowler <danny.fowler@nextroll.com>
2024-11-05 16:00:47 -0800

    fix: add user and host back to cli

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -48,7 +48,7 @@
 
   # Left prompt segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    # context                 # user@host
+    context                 # user@host
     dir                       # current directory
     vcs                       # git status
     # command_execution_time  # previous command duration
