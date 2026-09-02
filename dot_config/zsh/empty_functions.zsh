433192456c6fd598c2129032aa87d9aac147c8d1
Danny Fowler <danny.fowler@nextroll.com>
2026-09-02 09:41:10 -0700

    feat: add clone function using $DEFAULT_GITHUB_ORG

--- dot_config/zsh/empty_functions.zsh
+++ dot_config/zsh/empty_functions.zsh
@@ -29,6 +29,23 @@ gfind() {
     git log --all --grep="$1"
 }
 
+# Clone over ssh; bare repo name assumes $DEFAULT_GITHUB_ORG (set in ~/.profile)
+clone() {
+    local repo="${1%.git}"
+    if [[ -z "$repo" ]]; then
+        echo "usage: clone <repo|org/repo> [git clone args...]" >&2
+        return 1
+    fi
+    if [[ "$repo" != */* ]]; then
+        if [[ -z "$DEFAULT_GITHUB_ORG" ]]; then
+            echo "clone: DEFAULT_GITHUB_ORG unset - export it in ~/.profile, or pass <org/repo>" >&2
+            return 1
+        fi
+        repo="$DEFAULT_GITHUB_ORG/$repo"
+    fi
+    git clone "git@github.com:${repo}.git" "${@:2}"
+}
+
 ### AWS
 ECR_PROXY_ENDPOINT=`aws ecr get-authorization-token --output text --query 'authorizationData[].proxyEndpoint' --region us-west-2`
 _ecr_login() {
